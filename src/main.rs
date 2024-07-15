e509eb0acc0c3c1c0c2ee57247e563f15a8b8c63
Guilherme Felipe <guilherme.felipe@retta.com.br>
2024-07-15 15:43:39 -0300

    feat: começando a implementação da To Do list com TDD

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use clap::Parser;
 
 #[derive(Parser)]
@@ -5,10 +6,34 @@ struct Cli {
     command: String,
     key: String,
 }
+struct TodoList {
+    // true = to do, false = done
+    items: HashMap<String, bool>,
+}
+
+impl TodoList {
+    fn new() -> TodoList {
+        let items: HashMap<String, bool> =
+            HashMap::<String, bool>::new();
+
+        TodoList { items: items }
+    }
+}
 
 fn main() {
     let args = Cli::parse();
 
     println!("Command line: {} {}", args.command, args.key);
     
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn init_todo() {
+        let todo = TodoList::new();
+    }
 }
