49db83a921a6df19b64929f7ec577a82deab84b7
Guilherme Felipe <guilherme.felipe@retta.com.br>
2024-07-15 09:03:16 -0300

    feat: inicializando CLI

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    command: String,
+    key: String,
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    println!("Command line: {} {}", args.command, args.key);
+    
+}
